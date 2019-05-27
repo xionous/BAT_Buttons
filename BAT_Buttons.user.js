d59c30f28e81f57800e0acc9ba8c0951eb2913d8
Matthew Streeter <the.shadow909@gmail.com>
2019-05-27 17:25:42 -0500

    Update BAT_Buttons.user.js
    test

--- BAT_Buttons.user.js
+++ BAT_Buttons.user.js
@@ -3,7 +3,7 @@
 // @namespace all
 // @include https://shawprod.service-now.com/*
 // @author Matthew Streeter
-// @version 1.5.0
+// @version 1.5.1
 // @downloadURL https://github.com/xionous/BAT_Buttons/raw/master/BAT_Buttons.user.js
 // @updateURL https://github.com/xionous/BAT_Buttons/raw/master/BAT_Buttons.user.js
 // @grant none
