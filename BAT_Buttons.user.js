8c9101a933c85dfbc493761a40540a676d661982
Matthew Streeter <Matthew.Streeter@sjrb.ca>
2019-05-29 13:33:48 -0500

    fix version

--- BAT_Buttons.user.js
+++ BAT_Buttons.user.js
@@ -4,7 +4,7 @@
 // @include https://shawprod.service-now.com/*
 // @include https://shawqa.service-now.com/*
 // @author Matthew Streeter
-// @version 1.6.6
+// @version 1.6.7
 // @downloadURL https://github.com/xionous/BAT_Buttons/raw/master/BAT_Buttons.user.js
 // @updateURL https://github.com/xionous/BAT_Buttons/raw/master/BAT_Buttons.user.js
 // @grant none
